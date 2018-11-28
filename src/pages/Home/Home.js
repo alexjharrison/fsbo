4c123d4552619e5e55a0158937c8795f2f7410a8
Alex Harrison <alexjharrison@gmail.com>
2018-11-27 20:14:32 -0500

    added loads to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   state = {};
   render() {
     return (
-      <div>
+      <div id='home'>
         <div id='banner' style={{ backgroundImage: `url("${bgImage}")` }}>
           <div className='layover'>
             <h1>1638 Waters Edge Ln</h1>
@@ -16,9 +16,29 @@ class Home extends Component {
             </h2>
           </div>
         </div>
+        <hr />
         <div className='headline'>
           <h1>Beautiful Lakefront Home</h1>
           <h3>Located on Lake Anne, Minutes from Reston Town Center </h3>
+        </div>
+        <hr />
+        <div className='main-content'>
+          <div className='content-box'>
+            <img src='https://picsum.photos/500/400/?random' alt='random-img' />
+            <h1>4 Bed 3.5 Bath Property</h1>
+          </div>
+          <div className='content-box'>
+            <h1>Beautiful Lake Views From Living & Dining Rooms</h1>
+            <img src='https://picsum.photos/500/400/?random' alt='random-img' />
+          </div>
+          <div className='content-box'>
+            <img src='https://picsum.photos/500/400/?random' alt='random-img' />
+            <h1>Second Story Deck</h1>
+          </div>
+          <div className='content-box'>
+            <h1>Located in Northern VA's Most Attractive Communities</h1>
+            <img src='https://picsum.photos/500/400/?random' alt='random-img' />
+          </div>
         </div>
       </div>
     );
