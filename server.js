0b8f8903ade34f2acd6738665a1e7018dbcb7773
Alexander Harrison <aharrison@stifirestop.com>
2018-12-07 08:25:42 -0500

    fixed server code

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
-var serve = require('express-static');
 var http = require('http');
 var path = require('path');
 
-app.use(serve(__dirname  + '/build'))
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api',(req,res)=>{
+  console.log('api');
+  res.send('api');
+});
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
 
 var server = app.listen(3000, function() {
    console.log('server running');
