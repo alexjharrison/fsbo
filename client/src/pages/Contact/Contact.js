0d8d04863cf345b924d915e389023f773d5c7116
Alexander Harrison <aharrison@stifirestop.com>
2018-12-07 11:22:21 -0500

    testest

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -32,7 +32,7 @@ class Contact extends Component {
     render() {
         return (
         <div>
-            <h1>this is the best updated header</h1>
+            <h1>this is the even more best best updated header</h1>
             <form id="contact-form" onSubmit={this.handleSubmit}>
                 <label htmlFor="name">Name</label>
                 <br/>
