67acf5df68fc5daa614d43fc2e16a16d4c25b414
Alex Harrison <alexjharrison@gmail.com>
2018-12-10 20:43:38 -0500

    mvp achieved

--- client/src/pages/Details/Details.jsx
+++ client/src/pages/Details/Details.jsx
@@ -4,7 +4,22 @@ import React, { Component } from 'react';
 class Details extends Component {
   state = {};
   render() {
-    return <div>Details</div>;
+    return (
+      <div className='details'>
+        <div className='inner-details'>
+          <h1>Details</h1>
+          <h3>Floorplan</h3>
+          <a href='/Plotplan.jpg'>
+            <img src='/Plotplan.jpg' alt='blueprint' />
+          </a>
+          <h3>House Data</h3>
+          <iframe
+            title='house-data-sheet'
+            src='https://docs.google.com/spreadsheets/d/e/2PACX-1vSruabGu_zWU1RxDWWhn_C6V5MavHbptu2TspOohPgDvX_lpYwatc-qcKYztMhZkAwM97c2HqhhHKx-/pubhtml?widget=true&amp;headers=false'
+          />
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/pages/Neighborhood/Neighborhood.jsx
+++ client/src/pages/Neighborhood/Neighborhood.jsx
@@ -1,13 +1,78 @@
-import "./Neighborhood.css";
+import './Neighborhood.css';
 import React, { Component } from 'react';
+import { Card } from 'primereact/card';
 
 class Neighborhood extends Component {
-    state = {}
-    render() {
-        return (<div>
-            Neighborhood
-        </div>);
-    }
+  state = {};
+  render() {
+    return (
+      <div className='neighborhood'>
+        <div className='inner-neighbor'>
+          <h1>Lake Anne & Reston, VA</h1>
+          <img className='banner' src='/carousel/full/lake anne 1.jpg' alt='' />
+        </div>
+        <br />
+        <br />
+        <hr />
+        <h2>A Brief History of Reston & Lake Anne</h2>
+        <hr />
+        <iframe
+          title='youtube-reston'
+          src='https://www.youtube.com/embed/Qn5eHyn51nY'
+          frameborder='0'
+          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        />
+
+        <hr />
+        <h2>Enjoy Neighborhood Activities</h2>
+        <hr />
+        <div className='activities'>
+          <Card
+            title='Farmers Market'
+            subTitle='Saturdays Apr-Dec'
+            footer={
+              <a href='http://restonfarmersmarket.com'>
+                RestonFarmersMarket.com
+              </a>
+            }
+          >
+            <img
+              className='left-pic'
+              src='/farmer_sign.jpg'
+              alt='farmers market'
+            />
+            <div className='card-right'>
+              <img src='/farm2.jpg' alt='farmers market' />
+              <img src='/farm3.jpg' alt='farmers market' />
+            </div>
+          </Card>
+          <Card
+            title='Lake Anne Center'
+            subTitle='Shops and Dining a Few Steps Away'
+            footer={<a href='http://lakeanneplaza.com'>LakeAnnePlaza.com</a>}
+          >
+            <img class='card-pic' src='/lake-anne.png' alt='lake anne' />
+          </Card>
+          <Card
+            title='Reston Town Center'
+            subTitle='The Heart of the Region'
+            footer={
+              <a href='https://restontowncenter.com/'>RestonTownCenter.com</a>
+            }
+          >
+            <img
+              className='card-pic'
+              src='/reston_fountain.jpg'
+              alt='reston town center'
+            />
+            Card
+          </Card>
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default Neighborhood;
